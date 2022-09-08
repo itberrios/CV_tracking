ded6430a5e6e1cd886d171308b2cb46138d4342a
Isaac Berrios <60835780+itberrios@users.noreply.github.com>
2022-09-08 17:00:02 -0500

    added utils script

---
+++ kitti_tracker/kitti_utils.py
@@ -0,0 +1,9 @@
+'''
+This script contains various utility functions for working with the KITTI dataset
+'''
+
+def test_func(a, b):
+    ''' Trivial function to test imports in Google Colab '''
+    return a + b
+
+
