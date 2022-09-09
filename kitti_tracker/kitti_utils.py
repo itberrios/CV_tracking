a1e2b8e97520368942a16c3e268a52764e15750c
Isaac Berrios <60835780+itberrios@users.noreply.github.com>
2022-09-09 10:11:09 -0500

    added new function

--- kitti_tracker/kitti_utils.py
+++ kitti_tracker/kitti_utils.py
@@ -115,6 +115,62 @@ def xyz2camera(xyz, T, image=None, remove_outliers=True):
 
     return camera
 
+# ============================================================================================
+# pipeline functions
 
+def get_distances(image, velo_uvz, bboxes, draw=True):
+    ''' Obtains distance measurements for each detected object in the image 
+        Inputs:
+          image - input image for detection 
+          velo_uvz - LiDAR coordinates projected to camera reference
+          bboxes - xyxy bounding boxes form detections from yolov5 model output
+          draw - (_Bool) draw measured depths on image
+        Outputs:
+          image - input image with distances drawn at the center of each 
+                  bounding box
+        '''
+
+    # unpack LiDAR camera coordinates
+    u, v, z = velo_uvz
+
+    # get new output
+    bboxes_out = np.zeros((bboxes.shape[0], bboxes.shape[1] + 3))
+    bboxes_out[:, :bboxes.shape[1]] = bboxes
+
+    # iterate through all detected bounding boxes
+    for i, bbox in enumerate(bboxes):
+        pt1 = torch.round(bbox[0:2]).to(torch.int).numpy()
+        pt2 = torch.round(bbox[2:4]).to(torch.int).numpy()
+
+        # get center location of the object on the image
+        x_center = (pt1[1] + pt2[1]) / 2
+        y_center = (pt1[0] + pt2[0]) / 2
+
+        # now get the closest LiDAR points to the center
+        center_delta = np.abs(np.array((v, u)) 
+                              - np.array([[x_center, y_center]]).T)
+        
+        # choose coordinate pair with the smallest L2 norm
+        min_loc = np.argmin(np.linalg.norm(center_delta, axis=0))
+
+        # get LiDAR location in image/camera space
+        velo_depth = z[min_loc]; # LiDAR depth in camera space
+        velo_location = np.array([v[min_loc], u[min_loc], velo_depth])
+
+        # add velo (u, v, z) to bboxes
+        bboxes_out[i, -3:] = velo_location
+
+        # draw depth on image at center of each bounding box
+        if draw:
+            object_center = (np.round(y_center).astype(int), 
+                             np.round(x_center).astype(int))
+            cv2.putText(image, 
+                        '{0:.2f} m'.format(velo_depth), 
+                        object_center,
+                        cv2.FONT_HERSHEY_SIMPLEX, 
+                        0.5, 
+                        (255, 0, 0), 2, cv2.LINE_AA)    
+            
+    return image, bboxes_out
 
 
