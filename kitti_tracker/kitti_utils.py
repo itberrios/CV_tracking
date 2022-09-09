f0d7933fb9844ee1415baada853d0109325bead6
Isaac Berrios <60835780+itberrios@users.noreply.github.com>
2022-09-09 08:05:04 -0500

    added get_oxts()

--- kitti_tracker/kitti_utils.py
+++ kitti_tracker/kitti_utils.py
@@ -6,4 +6,15 @@ def test_func(a, b):
     ''' Trivial function to test imports in Google Colab '''
     return a + b
 
+# ============================================================================================
+# GPS/IMU functions
 
+def get_oxts(oxt_path):
+    ''' Obtains the oxt info from a single oxt path '''
+    with open(oxts_data_paths[0]) as f:
+        oxts = f.readlines()
+        
+    oxts = oxts[0].strip().split(' ')
+    oxts = np.array(oxts).astype(float)
+
+    return oxts
