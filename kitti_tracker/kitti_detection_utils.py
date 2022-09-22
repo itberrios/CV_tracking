7ed45fa76cb5a815bb2ddea85ff785845bc2a544
Isaac Berrios <60835780+itberrios@users.noreply.github.com>
2022-09-22 06:56:21 -0500

    Create kitti_detection_utils.py

---
+++ kitti_tracker/kitti_detection_utils.py
@@ -0,0 +1,128 @@
+'''
+Script that contains various detection functions for the KITTI dataset
+'''
+
+import numpy as np
+from kitti_utils import *
+
+
+def get_uvz_centers(image, velo_uvz, bboxes, draw=True):
+    ''' Obtains detected object centers projected to uvz camera coordinates. 
+        Starts by associating LiDAR uvz coordinates to detected object centers,
+        once a match is found, the coordiantes are transformed to the uvz
+        camera reference and added to the bboxes array.
+
+        NOTE: The image is modified in place so there is no need to return it.
+
+        Inputs:
+          image - input image for detection 
+          velo_uvz - LiDAR coordinates projected to camera reference
+          bboxes - xyxy bounding boxes form detections from yolov5 model output
+          draw - (_Bool) draw measured depths on image
+        Outputs:
+          bboxes_out - modified array containing the object centers projected
+                       to uvz image coordinates
+        '''
+
+    # unpack LiDAR camera coordinates
+    u, v, z = velo_uvz
+
+    # get new output
+    bboxes_out = np.zeros((bboxes.shape[0], bboxes.shape[1] + 3))
+    bboxes_out[:, :bboxes.shape[1]] = bboxes
+
+    # iterate through all detected bounding boxes
+    for i, bbox in enumerate(bboxes):
+        pt1 = torch.round(bbox[0:2]).to(torch.int).numpy()
+        pt2 = torch.round(bbox[2:4]).to(torch.int).numpy()
+
+        # get center location of the object on the image
+        obj_x_center = (pt1[1] + pt2[1]) / 2
+        obj_y_center = (pt1[0] + pt2[0]) / 2
+
+        # now get the closest LiDAR points to the center
+        center_delta = np.abs(np.array((v, u)) 
+                              - np.array([[obj_x_center, obj_y_center]]).T)
+        
+        # choose coordinate pair with the smallest L2 norm
+        min_loc = np.argmin(np.linalg.norm(center_delta, axis=0))
+
+        # get LiDAR location in image/camera space
+        velo_depth = z[min_loc]; # LiDAR depth in camera space
+        uvz_location = np.array([u[min_loc], v[min_loc], velo_depth])
+        
+        # add velo projections (u, v, z) to bboxes_out
+        bboxes_out[i, -3:] = uvz_location
+
+        # draw depth on image at center of each bounding box
+        # This is depth as perceived by the camera
+        if draw:
+            object_center = (np.round(obj_y_center).astype(int), 
+                             np.round(obj_x_center).astype(int))
+            cv2.putText(image, 
+                        '{0:.2f} m'.format(velo_depth), 
+                        object_center, # top left
+                        cv2.FONT_HERSHEY_SIMPLEX, 
+                        0.5, # font scale
+                        (255, 0, 0), 2, cv2.LINE_AA)    
+            
+    return bboxes_out
+
+  
+  
+  def get_detection_coordinates(image, bin_path, model, draw_boxes=True, draw_depth=True):
+    ''' Obtains detections for the input image, along with the coordinates of 
+        the detected object centers. The coordinate obtained are:
+            - Camera with depth --> uvz 
+            - LiDAR/velo --> xyz
+            - GPS/IMU --> xyz
+        Inputs:
+            image - rgb image to run detection on
+            bin_path - path to LiDAR bin file
+            model - detection model (this functions assumes a yolo5 model)
+                  - any detector can be used as long as it has the following attributes:
+                    show, xyxy
+        Output:
+            bboxes - array of detected bounding boxes, confidences, classes,
+            velo_uv - LiDAR points porjected to camera uvz coordinate frame
+            coordinates - array of all object center coordinates in the frames
+                          listed above
+        '''
+    ## 1. compute detections in the left image
+    detections = model(left_image)
+
+    # draw boxes on image
+    if draw_boxes:
+        detections.show() 
+
+    # get bounding box locations (x1,y1), (x2,y2) Prob, class
+    bboxes = detections.xyxy[0].cpu() # remove from GPU
+
+    # get LiDAR points and transform them to image/camera space
+    velo_uvz = project_velobin2uvz(bin_path, 
+                                   T_velo_cam2, 
+                                   left_image, 
+                                   remove_plane=True)
+
+    # get uvz centers for detected objects
+    bboxes = get_uvz_centers(image, 
+                             velo_uvz, 
+                             bboxes, 
+                             draw=draw_depth)
+
+    return bboxes, velo_uvz
+  
+  
+  
+  def get_imu_xyz(left_image, bin_path, model, T_cam2_imu):
+     ''' Obtains (x,y,z) location referenced to IMU '''
+     # get detections and object centers in uvz
+     bboxes, velo_uvz = get_detection_coordinates(left_image, bin_path, model)
+
+     # get transformed coordinates of object centers
+     uvz = bboxes[:, -3:]
+
+     # transform to IMU (u,v,z)
+     imu_xyz = transform_uvz(uvz, T_cam2_imu)
+      
+     return imu_xyz
